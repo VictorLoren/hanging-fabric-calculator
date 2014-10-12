f05d6da791ce4cc32685388b077474261bcb886b
Victor Geislinger <VictorLoren@users.noreply.github.com>
2014-10-11 22:14:52 -0700

    Update tule.py

--- tule.py
+++ tule.py
@@ -1,5 +1,9 @@
+#calculate the length of a hangin strand
 def calcLength(x,y,z=0):
-    return (x**2 + y**2 + z**2)**0.5
+    #For better accuracy, will need to compute the droop (catenary)
+    # Basic calculation of strand length (assume a straight line) 
+    strandLength = (x**2 + y**2 + z**2)**0.5
+    return strandLength
 
 #lengths are in feet
 L = 92; W=68; H=22; MidXY=(W/2,L/2)
@@ -33,7 +37,6 @@ def findTotal(widthOfStrand,z=0,printTotal=False):
     alongWidth = 0
     while(alongWidth <= W):
         newX,newY = (MidXY[0] - alongWidth,MidXY[1]-L)
-        
         # Length of strand needed (in yards)
         strandLength = calcLength(newX,newY,z)
         # Add Break point length
@@ -62,7 +65,7 @@ def findTotal(widthOfStrand,z=0,printTotal=False):
     if printTotal:
         print '\nTotal Length For Room: %.2f yards' %(2*total)
     # Return total length in yards and a list of strand lengths needed
-    return (2*total , strandLengths)
+    return (2*total , strandLengths.sort())
 
 def totalCost(costPerYard,widthOfStrandInFeet,drapingInFeet,printTotal=False):
     total = findTotal(widthOfStrandInFeet,drapingInFeet,printTotal)
